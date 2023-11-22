55bf43692a9a8efb4998915f8ccccc9d12ee2eb3
Samuel Christofel <samuelch2512@gmail.com>
2023-11-22 13:35:45 +0700

    pertama banget

---
+++ index.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class index {
+
+	public static void main(String[] args){
+		System.out.println("Welcome to Bloxcoffee! What would you like to order?\n
+         ");
+        int order = input.nextInt();
+	}
+}
